d5e6c85baec6f0ae9f2716679d21e4e89a3d72bf
mahopon <tan.cy@outlook.com>
2025-09-08 19:16:17 +0800

    Minor change

--- app/backend/app/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.auth.views import auth_router
-
-router = APIRouter()
-
-# Include all views/routes here from individual domains
-router.include_router(auth_router, prefix="/auth", tags=["Auth"])
