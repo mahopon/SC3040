0d256bbd0d7c3744ffb45526dcac9ecac495672b
mahopon <tan.cy@outlook.com>
2025-09-14 21:06:49 +0800

    Use reusable functions for retrieving rows

--- app/backend/app/auth/repository.py
+++ app/backend/app/auth/repository.py
@@ -2,27 +2,27 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select
 from .models import Auth, AuthSession
 from uuid import UUID
+from app.util.repository import get_by_field, db_add
+from typing import Optional
 
 
-def get_auth_by_email(*, db_session: Session, email: str) -> Auth | None:
-    stmt = select(Auth).where(Auth.email == email)
-    return db_session.execute(stmt).scalar_one_or_none()
+class AuthRepository:
+    def __init__(self, db_session: Session):
+        self.db_session = db_session
 
+    def get_by_email(self, email: str) -> Optional[Auth]:
+        return get_by_field(self.db_session, Auth, "email", email)
 
-def get_auth_by_id(*, db_session: Session, id: UUID) -> Auth | None:
-    stmt = select(Auth).where(Auth.id == id)
-    return db_session.execute(stmt).scalar_one_or_none()
+    def get_by_id(self, id: UUID) -> Optional[Auth]:
+        return get_by_field(self.db_session, Auth, "id", id)
 
+    def get_by_session_id(self, session_id: str) -> Optional[Auth]:
+        stmt = select(Auth, AuthSession).join(Auth.sessions).where(AuthSession.session_id == session_id)
+        auth = self.db_session.execute(stmt).scalar_one_or_none()
+        return auth
 
-def get_auth_by_session_id(*, db_session: Session, session_id: str) -> Auth | None:
-    stmt = select(Auth, AuthSession).join(Auth.sessions).where(AuthSession.session_id == session_id)
-    auth = db_session.execute(stmt).scalar_one_or_none()
-    return auth
+    def create_auth(self, auth_new: Auth) -> None:
+        db_add(self.db_session, auth_new)
 
-
-def create_auth(*, db_session: Session, auth_new: Auth) -> None:
-    db_session.add(auth_new)
-
-
-def create_session(*, db_session: Session, session_new: AuthSession) -> None:
-    db_session.add(session_new)
+    def create_session(self, session_new: AuthSession) -> None:
+        db_add(self.db_session, session_new)
