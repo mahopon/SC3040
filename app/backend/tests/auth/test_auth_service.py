7adfcbc0226ab5872f50c8b24ab0e596bfa05c24
mahopon <tan.cy@outlook.com>
2025-09-15 19:51:25 +0800

    Update unit test

--- app/backend/tests/auth/test_auth_service.py
+++ app/backend/tests/auth/test_auth_service.py
@@ -4,9 +4,10 @@ from unittest.mock import MagicMock
 from uuid import uuid4
 from app.auth.service import AuthService
 from app.auth.models import Auth, AuthSession
-from app.auth.schemas import AuthRegister, OAuthRegister, AuthPasswordUpdate
+from app.auth.schemas import AuthRegister, OAuthRegister, AuthPasswordUpdate, AuthLoginResponse
 from app.auth.exceptions import InvalidCredentials, EmailAlreadyExists, PasswordMismatch
 from datetime import datetime, timedelta
+from pydantic import SecretStr
 
 
 @pytest.fixture
@@ -24,11 +25,11 @@ def test_login_success(auth_service, mock_repo):
     auth.verify_password = MagicMock(return_value=True)
     mock_repo.get_by_email.return_value = auth
 
-    result = auth_service.login(email="test@test.com", password="P@ssw0rd123!")
+    result = auth_service.login(email="test@test.com", password=SecretStr("P@ssw0rd123!"))
 
-    assert result == auth
+    assert isinstance(result, AuthLoginResponse)
     auth.verify_password.assert_called_once_with("P@ssw0rd123!")
-    auth_service.repo.get_by_email.assert_called_once_with("test@test.com")
+    auth_service.repo.get_by_email.assert_called_once_with(email="test@test.com")
 
 
 def test_login_invalid(auth_service, mock_repo):
@@ -37,16 +38,17 @@ def test_login_invalid(auth_service, mock_repo):
     with pytest.raises(InvalidCredentials):
         auth_service.login(email="wrong@test.com", password="P@ssw0rd123!@")
 
-    auth_service.repo.get_by_email.assert_called_once_with("wrong@test.com")
+    auth_service.repo.get_by_email.assert_called_once_with(email="wrong@test.com")
 
 
 def test_retrieve_auth_by_session_success(auth_service, mock_repo):
-    auth = Auth(email="a@b.com")
+    id = uuid4()
+    auth = Auth(id=id, email="a@b.com")
     mock_repo.get_by_session_id.return_value = auth
 
-    result = auth_service.retrieve_auth_by_session(session_id="sess123")
+    result = auth_service.retrieve_auth_id_by_session(session_id="sess123")
 
-    assert result == auth
+    assert result == auth.id
     auth_service.repo.get_by_session_id.assert_called_once_with("sess123")
 
 
@@ -54,10 +56,10 @@ def test_retrieve_auth_by_session_invalid(auth_service, mock_repo):
     mock_repo.get_by_session_id.return_value = None
 
     with pytest.raises(InvalidCredentials):
-        auth_service.retrieve_auth_by_session(session_id="invalid")
+        auth_service.retrieve_auth_id_by_session(session_id="invalid")
 
 
-def test_register_web_success(auth_service, mock_repo):
+def test_register_success(auth_service, mock_repo):
     mock_repo.get_by_email.return_value = None
     auth_in = AuthRegister(
         email="new@test.com",
@@ -68,14 +70,13 @@ def test_register_web_success(auth_service, mock_repo):
     )
     auth_service.repo.create_auth = MagicMock()
 
-    result = auth_service.register_web(auth_in=auth_in)
+    auth_service.register(auth_in=auth_in)
 
-    assert isinstance(result, Auth)
     auth_service.repo.create_auth.assert_called_once()
     auth_service.repo.get_by_email.assert_called_once_with(auth_in.email)
 
 
-def test_register_web_existing_email(auth_service, mock_repo):
+def test_register_existing_email(auth_service, mock_repo):
     mock_repo.get_by_email.return_value = Auth(email="exists@test.com")
     auth_in = AuthRegister(
         email="exists@test.com",
@@ -86,7 +87,7 @@ def test_register_web_existing_email(auth_service, mock_repo):
     )
 
     with pytest.raises(EmailAlreadyExists):
-        auth_service.register_web(auth_in=auth_in)
+        auth_service.register(auth_in=auth_in)
 
     auth_service.repo.get_by_email.assert_called_once_with("exists@test.com")
 
@@ -95,7 +96,7 @@ def test_register_oauth(auth_service, mock_repo):
     auth_in = OAuthRegister(email="oauth@test.com", name="User")
     auth_service.repo.create_auth = MagicMock()
 
-    result = auth_service.register_oauth(auth_in=auth_in)
+    result = auth_service._register_oauth(auth_in=auth_in)
 
     assert isinstance(result, Auth)
     auth_service.repo.create_auth.assert_called_once()
@@ -103,9 +104,9 @@ def test_register_oauth(auth_service, mock_repo):
 
 def test_create_session(auth_service, mock_repo):
     auth = Auth(email="test@test.com")
-    auth_service.repo.create_session = MagicMock()
+    auth_service.repo._create_session = MagicMock()
 
-    result = auth_service.create_session(auth=auth)
+    result = auth_service._create_session(auth=auth)
 
     assert isinstance(result, AuthSession)
     assert isinstance(result.session_id, str)
