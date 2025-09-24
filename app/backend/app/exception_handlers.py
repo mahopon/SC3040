31305a4cb606c66a1ba699603f514bdb726aaae0
mahopon <tan.cy@outlook.com>
2025-09-24 23:26:24 +0800

    Added new exception handlers for profile

--- app/backend/app/exception_handlers.py
+++ app/backend/app/exception_handlers.py
@@ -4,7 +4,14 @@ from app.auth.exception_handlers import (
     password_mismatch_exception_handler,
 )
 from app.auth.exceptions import InvalidCredentials, EmailAlreadyExists, PasswordMismatch
-
+from app.profile.exception_handlers import (
+    profile_exists_exception_handler,
+    profile_not_exists_exception_handler,
+    profile_onboarded_exception_handler,
+)
+from app.profile.exceptions import ProfileAlreadyExists, ProfileAlreadyOnboarded, ProfileNotExists
+from app.petcaretaker.exception_handlers import petcaretaker_not_found_exception_handler
+from app.petcaretaker.exceptions import PetCareTakerNotFound
 from fastapi import FastAPI, status
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
@@ -18,7 +25,15 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
 
 def register_exception_handlers(app: FastAPI) -> None:
+    # Global
+    app.add_exception_handler(RequestValidationError, validation_exception_handler)  # type: ignore[arg-type]
+    # Auth
     app.add_exception_handler(InvalidCredentials, invalid_credentials_exception_handler)  # type: ignore[arg-type]
     app.add_exception_handler(EmailAlreadyExists, email_exists_exception_handler)  # type: ignore[arg-type]
     app.add_exception_handler(PasswordMismatch, password_mismatch_exception_handler)  # type: ignore[arg-type]
-    app.add_exception_handler(RequestValidationError, validation_exception_handler)  # type: ignore[arg-type]
+    # Profile
+    app.add_exception_handler(ProfileNotExists, profile_not_exists_exception_handler)  # type: ignore[arg-type]
+    app.add_exception_handler(ProfileAlreadyOnboarded, profile_onboarded_exception_handler)  # type: ignore[arg-type]
+    app.add_exception_handler(ProfileAlreadyExists, profile_exists_exception_handler)  # type: ignore[arg-type]
+    # PetCareTaker
+    app.add_exception_handler(PetCareTakerNotFound, petcaretaker_not_found_exception_handler)
