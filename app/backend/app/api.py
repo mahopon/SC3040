4c4de86cabbd9ed82e2d52095e2540fc5b22bd15
mahopon <tan.cy@outlook.com>
2025-09-25 02:16:23 +0800

    Add new routers

--- app/backend/app/api.py
+++ app/backend/app/api.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter
 from app.auth.views import auth_router
-
-# from app.profile.views import profile_router
+from app.profile.views import profile_router
+from app.onboarding.views import onboarding_router
+from app.service.views import service_router
 
 router = APIRouter()
 
 # Include all views/routes here from individual domains
 router.include_router(auth_router, prefix="/auth", tags=["Auth"])
-# router.include_router(profile_router, prefix="/profile", tags=["Profile"])
+router.include_router(profile_router, prefix="/profile", tags=["Profile"])
+router.include_router(onboarding_router, prefix="/onboarding", tags=["Onboarding"])
+router.include_router(service_router, prefix="/service", tags=["Service"])
