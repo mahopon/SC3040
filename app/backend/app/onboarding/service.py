d97dd719a2b3d8c8ab19c370255d34bcb5672db0
Chong Yao <tan.cy01@outlook.com>
2025-09-25 16:43:46 +0800

    Add petowner and petcaretaker deletion for onboarding

--- app/backend/app/onboarding/service.py
+++ app/backend/app/onboarding/service.py
@@ -2,6 +2,7 @@ from .protocols import InternalOnboardingService
 from app.profile.protocols import ExternalProfileService as ProfileService
 from app.profile.schemas import ProfileOnboardRequest, ProfileOnboard
 from app.profile.enums import Role
+from app.profile.exceptions import ProfileAlreadyOnboarded
 from app.petowner.protocols import ExternalPetOwnerService as PetOwnerService
 from app.petowner.schemas import PetOwnerCreate
 from app.petcaretaker.protocols import ExternalPetCareTakerService as PetCareTakerService
@@ -33,6 +34,8 @@ class OnboardingService(InternalOnboardingService):
         profile_onboard = ProfileOnboard(**onboard_req.model_dump(exclude={"yoe"}))
         self.profile_service.onboard_profile(profile_id=profile_id, profile_update=profile_onboard)
 
+        self.petowner_service.delete_pet_owner(petowner_id=profile_id)
+        self.petcaretaker_service.delete_petcaretaker(petcaretaker_id=profile_id)
         if profile_onboard.type == Role.PetOwner.value:
             new_petowner = PetOwnerCreate(id=profile_id)
             self.petowner_service.create_pet_owner(petowner_new=new_petowner)
@@ -43,11 +46,17 @@ class OnboardingService(InternalOnboardingService):
             self.petcaretaker_service.create_petcaretaker(petcaretaker_new=new_petcaretaker)
 
     def onboard_pet(self, *, profile_id: UUID, new_pet_req: PetCreate) -> None:
+        if self.profile_service.check_onboarding_status(profile_id=profile_id):
+            raise ProfileAlreadyOnboarded("Profile has completed onboarding")
         self.pet_service.create_pet(owner_id=profile_id, pet_new=new_pet_req)
 
     def onboard_offered_services(self, *, profile_id: UUID, offered_services: List[OfferedServiceCreate]) -> None:
+        if self.profile_service.check_onboarding_status(profile_id=profile_id):
+            raise ProfileAlreadyOnboarded("Profile has completed onboarding")
         for offered_service in offered_services:
             self.service_service.create_offered_service(profile_id=profile_id, offered_service_req=offered_service)
 
     def complete_onboard(self, *, profile_id: UUID) -> None:
+        if self.profile_service.check_onboarding_status(profile_id=profile_id):
+            raise ProfileAlreadyOnboarded("Profile has completed onboarding")
         self.profile_service.complete_onboard(profile_id=profile_id)
