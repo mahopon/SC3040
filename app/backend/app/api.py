9908c0fed535b7b9c54c9b81e31f001a82af05c8
Chong Yao <tan.cy01@outlook.com>
2025-09-30 16:04:40 +0800

    Add router for location

--- app/backend/app/api.py
+++ app/backend/app/api.py
@@ -3,6 +3,7 @@ from app.auth.views import auth_router
 from app.profile.views import profile_router
 from app.onboarding.views import onboarding_router
 from app.service.views import service_router
+from app.location.views import location_router
 
 router = APIRouter()
 
@@ -11,3 +12,4 @@ router.include_router(auth_router, prefix="/auth", tags=["Auth"])
 router.include_router(profile_router, prefix="/profile", tags=["Profile"])
 router.include_router(onboarding_router, prefix="/onboarding", tags=["Onboarding"])
 router.include_router(service_router, prefix="/service", tags=["Service"])
+router.include_router(location_router, prefix="/location", tags=["Location"])
