a1bc356a391b592e40731d8ad8c9269a8919334e
Chong Yao <tan.cy01@outlook.com>
2025-09-30 21:21:35 +0800

    Add offered service router

--- app/backend/app/api.py
+++ app/backend/app/api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from app.auth.views import auth_router
 from app.profile.views import profile_router
 from app.onboarding.views import onboarding_router
-from app.service.views import service_router
+from app.service.views import service_router, offered_service_router
 from app.location.views import location_router
 
 router = APIRouter()
@@ -12,4 +12,5 @@ router.include_router(auth_router, prefix="/auth", tags=["Auth"])
 router.include_router(profile_router, prefix="/profile", tags=["Profile"])
 router.include_router(onboarding_router, prefix="/onboarding", tags=["Onboarding"])
 router.include_router(service_router, prefix="/service", tags=["Service"])
+router.include_router(offered_service_router, prefix="/offered-service", tags=["Offered Service"])
 router.include_router(location_router, prefix="/location", tags=["Location"])
