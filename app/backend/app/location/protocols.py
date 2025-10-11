c24f30c426e5ac69641bcd5fde03a7b2b1504851
Chong Yao <tan.cy01@outlook.com>
2025-10-12 05:15:49 +0800

    Fix protocol

--- app/backend/app/location/protocols.py
+++ app/backend/app/location/protocols.py
@@ -1,9 +1,10 @@
 from typing import Protocol, List
 from .schemas import Location as LocationDTO
+from .models import Location
 
 
 class ExternalLocationService(Protocol):
-    def get_filtered_locations(self, *, location_ids: List[int]) -> List[LocationDTO]: ...
+    def get_filtered_locations(self, *, location_ids: List[int]) -> List[Location]: ...
 
 
 class InternalLocationService(ExternalLocationService, Protocol):
