9e32940c1fd303f41f29135e39428f76f0f231c8
Chong Yao <tan.cy01@outlook.com>
2025-10-12 04:07:36 +0800

    Add review domain

--- app/backend/app/review/models.py
+++ app/backend/app/review/models.py
@@ -7,9 +7,9 @@ class Review(Base):
     service_booking_id: Mapped[int] = mapped_column(
         ForeignKey("service_booking.id", ondelete="CASCADE"), primary_key=True
     )
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     description: Mapped[str] = mapped_column(nullable=False)
     rating: Mapped[int] = mapped_column(nullable=False)
     isAnonymouse: Mapped[bool] = mapped_column(nullable=False, default=True)
 
-    service_booking: Mapped["ServiceBooking"] = relationship(back_populates="review")  # noqa
+    service_booking: Mapped["ServiceBooking"] = relationship(back_populates="review", lazy="joined")  # noqa
